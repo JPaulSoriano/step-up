dd0ce2d0799264546dcb3275e28f69c230faf0fd
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2021-11-07 13:12:01 +0800

    code

---
+++ invitation/generate.php
@@ -0,0 +1,24 @@
+<?php
+
+// database connection
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$db = 'demo';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass , $db) or die($conn);	
+
+// create function for generate random password
+function generate_password($len = 8){
+	$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	$code = substr( str_shuffle( $chars ), 0, $len );
+	return $code;
+}
+
+// insert into database
+if(isset($_POST['generate'])) {
+	$code = generate_password();
+	mysqli_query($conn, "insert into invitation_codes (created_by, created_date, code) values (1, now(), '$code')");
+	header("location: create.php");
+}
+
+?>
